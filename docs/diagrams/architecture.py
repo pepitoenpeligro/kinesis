3f6a7aad1efd31fa2476e499996a3413f1f05955
Jose Antonio <joseant.cg@outlook.com>
2023-02-18 22:44:43 +0100

    ADD diagram

---
+++ docs/diagrams/architecture.py
@@ -0,0 +1,25 @@
+from diagrams import Cluster, Diagram, Digraph
+from diagrams.aws.compute import ECS, EKS, Lambda
+from diagrams.aws.database import Redshift
+from diagrams.aws.integration import SQS, SNS, SimpleNotificationServiceSnsEmailNotification
+from diagrams.aws.storage import S3
+
+from diagrams.aws.analytics import Kinesis, KinesisDataAnalytics, KinesisDataStreams
+
+
+with Diagram(name="Dementia Realtime Analytics", show=True, direction="LR" ):
+        
+        kinesisInput = KinesisDataStreams("InputStram")
+        kinesisAnalytics = KinesisDataAnalytics("Analytics")
+
+        topic = SNS("Anomaly Notification")
+        email = SimpleNotificationServiceSnsEmailNotification("Email")
+        
+
+        with Cluster("Post Processing"):
+            output = [Lambda("Fan Out"), KinesisDataStreams("OutputStream")] 
+            kinesisAnalytics >> output
+        
+        kinesisInput >> kinesisAnalytics
+        output[0] >> topic >> email
+
